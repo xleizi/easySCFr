26d00a1b043f5568003bbd193210c07c77af4bda
xleizi <cuilei798@qq.com>
2024-07-24 20:47:26 +0800

    0.2.0

--- R/save.R
+++ R/save.R
@@ -49,9 +49,7 @@ X_to_h5 <- function(
     sce_X <- as(sce_X, "RsparseMatrix")
   }
   if ("matrix" %in% class(sce_X) | attr(class(sce_X), "package") == "BPCells") {
-    print("==============")
     sce_X <- Matrix::t(sce_X)
-    print("--------------")
   }
   handle_data_splitting(sce_X, h5data, data_name, split_save, max_cells_per_subset)
 }
